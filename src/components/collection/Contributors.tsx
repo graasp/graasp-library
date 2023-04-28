d223907ba93f1eef0b5b9c81c7bc340900f2f3af
spaenleh <spaenleh@gmail.com>
2023-04-28 22:07:43 +0200

    fix: use Stack in place of simple div

--- src/components/collection/Contributors.tsx
+++ src/components/collection/Contributors.tsx
@@ -1,18 +1,23 @@
 import { List } from 'immutable';
-import PropTypes from 'prop-types';
 
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import AvatarGroup from '@mui/lab/AvatarGroup';
-import { Avatar, Tooltip, Typography } from '@mui/material';
+import { Avatar, Stack, Tooltip, Typography } from '@mui/material';
 
+import { MemberRecord } from '@graasp/sdk/frontend';
 import { LIBRARY } from '@graasp/translations';
 
 import { buildContributorId } from '../../config/selectors';
 import { getAvatar } from '../../utils/layout';
 
-const Contributors = ({ contributors, displayContributors }) => {
+type Props = {
+  contributors: List<MemberRecord>;
+  displayContributors: boolean;
+};
+
+const Contributors = ({ contributors, displayContributors }: Props) => {
   const { t } = useTranslation();
 
   if (!contributors || contributors.isEmpty()) {
@@ -24,20 +29,15 @@ const Contributors = ({ contributors, displayContributors }) => {
   }
 
   return (
-    <div
-      style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}
-    >
+    <Stack direction="row" alignItems="center">
       <Typography variant="subtitle2" mx={2} color="primary" fontWeight="bold">
         {t(LIBRARY.CONTRIBUTORS_TITLE)}
       </Typography>
       <AvatarGroup max={8}>
         {contributors.map((contributor) => {
-          const {
-            id,
-            name: contributorName,
-            image: contributorAvatar,
-          } = contributor;
-          const avatar = getAvatar(contributorAvatar);
+          const { id, name: contributorName } = contributor;
+          // todo: get avatar url
+          const avatar = getAvatar(undefined);
           return (
             <Tooltip title={contributorName} key={id} arrow>
               <Avatar
@@ -49,17 +49,8 @@ const Contributors = ({ contributors, displayContributors }) => {
           );
         })}
       </AvatarGroup>
-    </div>
+    </Stack>
   );
-};
-
-Contributors.propTypes = {
-  contributors: PropTypes.instanceOf(List),
-  displayContributors: PropTypes.bool.isRequired,
-};
-
-Contributors.defaultProps = {
-  contributors: List(),
 };
 
 export default Contributors;
