0db0f1c6ae75346ac5223780bcc9a89797bf7c1b
spaenleh <spaenleh@gmail.com>
2023-05-09 09:53:38 +0200

    fix: navigation using href

--- src/components/common/HeaderNavigation.tsx
+++ src/components/common/HeaderNavigation.tsx
@@ -5,8 +5,6 @@ import React from 'react';
 
 import { Box, Typography, styled } from '@mui/material';
 
-import { redirect } from '@graasp/sdk';
-
 import { APP_NAME, HOST_MAP } from '../../config/constants';
 import { HEADER_LOGO_HEIGHT } from '../../config/cssStyles';
 
@@ -77,13 +75,7 @@ export function usePlatformNavigation(
     const url = hostsMapper[platform]?.(itemId);
     const href = url ?? '#';
     return {
-      onClick: () => redirect(href),
-      onMouseDown: (event: React.MouseEvent) => {
-        if (event.button !== 1 || url === undefined) {
-          return;
-        }
-        window.open(href, '_blank');
-      },
+      href,
     };
   };
 }
