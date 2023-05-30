eba16b2a68649186e2e53955a6a48bf914dc57cb
Clément <clement.charmillot@epfl.ch>
2023-05-30 22:16:51 +0200

    fix: remove dead code

--- src/components/home/Search.tsx
+++ src/components/home/Search.tsx
@@ -30,81 +30,53 @@ const Search: React.FC<SearchProps> = ({ handleClick, isLoading }) => {
     }
   };
 
-  /*
   const handleSearchOnClick = (
     event: React.KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>,
   ) => {
     if (event.code === 'Enter') {
       handleClick(searchInput);
     }
-  }; 
-  */
+  };
 
   return (
-    <>
-      <Paper
-        sx={{
-          boxShadow: 'none',
-          py: 1,
-          pl: 2,
-          display: 'flex',
-          alignItems: 'center',
-          width: '100%',
-          my: 1,
-          borderRadius: 2,
+    <Paper
+      sx={{
+        boxShadow: 'none',
+        py: 1,
+        pl: 2,
+        display: 'flex',
+        alignItems: 'center',
+        width: '100%',
+        my: 1,
+        borderRadius: 2,
+      }}
+    >
+      <InputBase
+        // search on click
+        onKeyUp={handleSearch}
+        id={HOME_SEARCH_ID}
+        disabled={isLoading}
+        sx={{ m: 1 }}
+        placeholder={t(LIBRARY.SEARCH_PLACEHOLDER)}
+        fullWidth
+        margin="none"
+        inputProps={{
+          'aria-label': LIBRARY.SEARCH_ARIA_LABEL,
         }}
+        onChange={handleChange}
+        onKeyDown={handleSearchOnClick}
+      />
+      <Divider sx={{ m: 1 }} orientation="vertical" />
+      <IconButton
+        id={HOME_SEARCH_BUTTON_ID}
+        color="primary"
+        sx={{ p: 1 }}
+        aria-label={t(LIBRARY.SEARCH_BUTTON_ARIA_LABEL)}
+        onClick={handleSearch}
       >
-        <InputBase
-          // search on click
-          onKeyUp={handleSearch}
-          id={HOME_SEARCH_ID}
-          disabled={isLoading}
-          sx={{ m: 1 }}
-          placeholder={t(LIBRARY.SEARCH_PLACEHOLDER)}
-          fullWidth
-          margin="none"
-          inputProps={{
-            'aria-label': LIBRARY.SEARCH_ARIA_LABEL,
-          }}
-          onChange={handleChange}
-        />
-        <Divider sx={{ m: 1 }} orientation="vertical" />
-        <IconButton
-          id={HOME_SEARCH_BUTTON_ID}
-          color="primary"
-          sx={{ p: 1 }}
-          aria-label={t(LIBRARY.SEARCH_BUTTON_ARIA_LABEL)}
-          onClick={handleSearch}
-        >
-          <SearchIcon />
-        </IconButton>
-      </Paper>
-      {/*
-      <FormControl component="fieldset" ml={1} mb={2}>
-        <FormLabel component="legend">
-          {t(LIBRARY.SEARCH_RANGE_LABEL)}
-        </FormLabel>
-        <RadioGroup row value={range} onChange={handleRangeChange}>
-          {Object.values(SEARCH_RANGES).map((entry) => (
-            <FormControlLabel
-              key={entry.title}
-              value={entry.value}
-              control={<Radio color="primary" />}
-              label={t(entry.title)}
-              id={buildSearchRangeOptionId(entry.value)}
-            />
-          ))}
-          {/*
-            TODO: prompt users hints about how to use multiple keywords
-            <Tooltip title="Use | or & for union/intersection of multiple keywords if choose 'Tag' or 'All'">
-            <IconButton>
-              <HelpIcon />
-            </IconButton>
-          </Tooltip>
-        </RadioGroup>
-      </FormControl>
-      */}
-    </>
+        <SearchIcon />
+      </IconButton>
+    </Paper>
   );
 };
 
