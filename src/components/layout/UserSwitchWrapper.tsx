e634c4f2dbb705bce3420cd5d225b85253208177
LinaYahya <linaebe0@gmail.com>
2023-10-09 16:41:40 +0200

    build: add liked items within header

--- src/components/layout/UserSwitchWrapper.tsx
+++ src/components/layout/UserSwitchWrapper.tsx
@@ -1,11 +1,14 @@
 import dynamic from 'next/dynamic';
+import Link from 'next/link';
 
 import { FC, useContext } from 'react';
 
 import Box from '@mui/material/Box';
 
 import { MEMBER_AVATAR_ICON_SIZE } from '../../config/constants';
+import { useLibraryTranslation } from '../../config/i18n';
 import { MEMBER_PROFILE_ROUTE, SIGN_IN_ROUTE } from '../../config/paths';
+import LIBRARY from '../../langs/constants';
 import { QueryClientContext } from '../QueryClientContext';
 import MemberAvatar from './MemberAvatar';
 
@@ -24,24 +27,35 @@ const UserSwitchWrapper: FC<Props> = ({ ButtonContent }) => {
   const { hooks, mutations } = useContext(QueryClientContext);
   const { data: member, isLoading } = hooks.useCurrentMember();
   const { mutateAsync: signOut } = mutations.useSignOut();
+  const { t } = useLibraryTranslation();
 
   return (
-    <Box mr={1}>
-      <GraaspUserSwitch
-        ButtonContent={ButtonContent}
-        signOut={signOut}
-        currentMember={member}
-        isCurrentMemberLoading={isLoading}
-        profilePath={MEMBER_PROFILE_ROUTE}
-        redirectPath={SIGN_IN_ROUTE}
-        renderAvatar={(m) => (
-          <MemberAvatar
-            sx={{ mr: 1 }}
-            size={MEMBER_AVATAR_ICON_SIZE}
-            memberId={m?.id}
-          />
-        )}
-      />
+    <Box sx={{ display: 'flex', gap: '24px', alignItems: 'center' }}>
+      {member?.id && (
+        <Link
+          href="/my-list"
+          style={{ textDecoration: 'none', color: 'white' }}
+        >
+          {t(LIBRARY.LIKED_ITEMS)}
+        </Link>
+      )}
+      <Box mr={1}>
+        <GraaspUserSwitch
+          ButtonContent={ButtonContent}
+          signOut={signOut}
+          currentMember={member}
+          isCurrentMemberLoading={isLoading}
+          profilePath={MEMBER_PROFILE_ROUTE}
+          redirectPath={SIGN_IN_ROUTE}
+          renderAvatar={(m) => (
+            <MemberAvatar
+              sx={{ mr: 1 }}
+              size={MEMBER_AVATAR_ICON_SIZE}
+              memberId={m?.id}
+            />
+          )}
+        />
+      </Box>
     </Box>
   );
 };
