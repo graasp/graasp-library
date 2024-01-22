5bfacbd55f16a13f25b70ca1e54f853b7fac6c15
LinaYahya <linaebe0@gmail.com>
2024-01-22 12:11:57 +0100

    feat: create a component to handle show less more content

--- src/components/member/BioSection.tsx
+++ src/components/member/BioSection.tsx
@@ -1,12 +1,12 @@
 import dynamic from 'next/dynamic';
 import { SocialLinks } from 'social-links';
 
-import React, { useContext, useEffect, useRef, useState } from 'react';
+import React, { useContext } from 'react';
 
 import FacebookIcon from '@mui/icons-material/Facebook';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import TwitterIcon from '@mui/icons-material/Twitter';
-import { Box, Button, Link, Stack, Typography, styled } from '@mui/material';
+import { Box, Link, Stack, Typography, styled } from '@mui/material';
 
 import { Member, ThumbnailSize } from '@graasp/sdk';
 
@@ -15,8 +15,8 @@ import { useLibraryTranslation } from '../../config/i18n';
 import { publicProfileAccountPath } from '../../config/paths';
 import LIBRARY from '../../langs/constants';
 import { QueryClientContext } from '../QueryClientContext';
-import ContentDescription from '../collection/ContentDescription';
 import BackButton from '../common/BackButton';
+import ShowLessAndMoreContent from '../common/ShowLessAndMoreContent';
 
 const Avatar = dynamic(() => import('@graasp/ui').then((mod) => mod.Avatar), {
   ssr: false,
@@ -43,42 +43,17 @@ const StyledBox = styled(Box)(({ theme }) => ({
   },
 }));
 
-const contentLinesToShow = 3;
-
 const BioSection = ({ id, memberData, isOwnProfile }: Props) => {
   const { t } = useLibraryTranslation();
-  const [collapsedDescription, setCollapsedDescription] = useState(true);
-  const [isBioClamped, setIsBioClamped] = useState(false);
 
   const { hooks } = useContext(QueryClientContext);
   const { data: publicProfile } = hooks.usePublicProfile(id || '');
-
-  const contentRef = useRef<HTMLDivElement>(null);
 
   const { data: authorUrl, isLoading: isLoadingAuthorAvatar } =
     hooks.useAvatarUrl({
       id,
       size: ThumbnailSize.Small,
     });
-  const handleShowMoreButton = () => {
-    setCollapsedDescription(!collapsedDescription);
-  };
-
-  useEffect(() => {
-    // check if text is more than content number of lines to control show less/more button
-    if (contentRef.current && publicProfile?.bio) {
-      const lineHeight = parseInt(
-        window
-          .getComputedStyle(contentRef.current)
-          .getPropertyValue('line-height'),
-        10,
-      );
-      const elementHeight = contentRef.current.offsetHeight;
-      const calculatedLines = Math.floor(elementHeight / lineHeight);
-
-      setIsBioClamped(calculatedLines >= contentLinesToShow);
-    }
-  }, [contentRef, publicProfile?.bio]);
 
   return (
     <Stack
@@ -121,28 +96,7 @@ const BioSection = ({ id, memberData, isOwnProfile }: Props) => {
               <Link href={publicProfileAccountPath}>{t(LIBRARY.EDIT)}</Link>
             )}
           </Box>
-
-          <Box my={2} width="100%">
-            <Box ref={contentRef}>
-              <ContentDescription
-                content={publicProfile?.bio}
-                collapsed={collapsedDescription}
-                numberOfLinesToShow={contentLinesToShow}
-              />
-            </Box>
-            {publicProfile?.bio && isBioClamped && (
-              <Button
-                sx={{ minWidth: 'max-content' }}
-                size="small"
-                onClick={handleShowMoreButton}
-              >
-                {collapsedDescription
-                  ? t(LIBRARY.SUMMARY_DESCRIPTION_SHOW_MORE)
-                  : t(LIBRARY.SUMMARY_DESCRIPTION_SHOW_LESS)}
-              </Button>
-            )}
-          </Box>
-
+          <ShowLessAndMoreContent content={publicProfile?.bio} />
           {(publicProfile?.facebookID ||
             publicProfile?.linkedinID ||
             publicProfile?.twitterID) && (
