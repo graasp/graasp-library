93ca5b3ec0386867298e84477d87adede50649b8
kim <kim.phanhoang@epfl.ch>
2024-04-26 15:31:47 +0200

    feat: add oer info page (to be done)

---
+++ app/oer/page.tsx
@@ -0,0 +1,39 @@
+import { Suspense } from 'react';
+import { dehydrate } from 'react-query/core';
+
+import Hydrate from '../../src/components/HydrateClient';
+import Wrapper from '../../src/components/common/Wrapper';
+import OERInformation from '../../src/components/pages/OERInformation';
+import { APP_AUTHOR } from '../../src/config/constants';
+import getQueryClient from '../../src/config/get-query-client';
+import en from '../../src/langs/en.json';
+import { buildSeo } from '../seo';
+
+export async function generateMetadata() {
+  // todo: get lang from location and crawler
+  // question: how to get language from
+  // @ts-ignore
+  const t = (s: string): string => en[s];
+
+  return buildSeo({
+    title: t('OER information'),
+    description: t('Information about Open Educational Resources OER'),
+    author: APP_AUTHOR,
+  });
+}
+
+const Page = async () => {
+  const queryClient = getQueryClient();
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <Hydrate state={dehydratedState}>
+      <Suspense>
+        <Wrapper dehydratedState={dehydratedState}>
+          <OERInformation />
+        </Wrapper>
+      </Suspense>
+    </Hydrate>
+  );
+};
+export default Page;
