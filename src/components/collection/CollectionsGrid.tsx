621a806b0704b7cfce84d60a38219fddf5e118d4
spaenleh <spaenleh@gmail.com>
2024-10-23 14:22:13 +0200

    fix: update mui to v6

--- src/components/collection/CollectionsGrid.tsx
+++ src/components/collection/CollectionsGrid.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
-import { Breakpoint } from '@mui/material';
+import { Breakpoint, Grid2 as Grid } from '@mui/material';
 import Container from '@mui/material/Container';
 import Skeleton from '@mui/material/Skeleton';
 import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Unstable_Grid2';
 
 import { useLibraryTranslation } from '../../config/i18n';
 import { buildCollectionCardGridId } from '../../config/selectors';
@@ -41,7 +40,7 @@ const CollectionsGrid = ({
         id={id}
       >
         {Array.from({ length: 4 }, (_, idx) => idx).map((idx) => (
-          <Grid key={idx} xs={6} sm={4} md={3} lg={3} xl={2}>
+          <Grid key={idx} size={{ xs: 6, sm: 4, md: 3, lg: 3, xl: 2 }}>
             <Skeleton height={height} sx={{ transform: 'unset' }} />
           </Grid>
         ))}
@@ -66,11 +65,13 @@ const CollectionsGrid = ({
           {collections?.map((collection) => (
             <Grid
               key={collection.id}
-              xs={6}
-              sm={4}
-              md={3}
-              lg={3}
-              xl={2}
+              size={{
+                xs: 6,
+                sm: 4,
+                md: 3,
+                lg: 3,
+                xl: 2,
+              }}
               id={buildCollectionCardGridId(collection.id)}
             >
               <CollectionCard
