39ce5c57426e6d7fd7db3ec5a8ebf7cd616e9810
kim <kim.phanhoang@epfl.ch>
2025-01-08 10:43:50 +0100

    refactor: apply PR requested changes

--- src/components/common/CollapsibleText/CollapsibleText.stories.tsx
+++ src/components/common/CollapsibleText/CollapsibleText.stories.tsx
@@ -6,6 +6,11 @@ import { CollapsibleText } from './CollapsibleText';
 const meta = {
   title: 'Common/CollapsibleText',
   component: CollapsibleText,
+  args: {
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus scelerisque, nibh eu dapibus dignissim, eros orci tristique ipsum, id interdum ex neque a dui. Quisque pretium aliquam dui, pulvinar venenatis sem consectetur non. Sed felis mi, viverra sit amet blandit sed, vestibulum eu purus. Nulla eget tellus sodales, Vivamus scelerisque, nibh eu dapibus dignissim, eros orci tristique ipsum, id interdum ex neque a dui. Quisque pretium aliquam dui, pulvinar venenatis sem consectetur non. Sed felis mi, viverra sit amet blandit sed, vestibulum eu purus. Nulla eget tellus sodales, volutpat nulla in, blandit tellus. Fusce congue vitae elit ac scelerisque. Maecenas fringilla ipsum in enim volutpat dignissim. Aenean convallis urna vel nibh semper faucibus. Nunc non pellentesque nibh. Sed tempor, erat sit amet volutpat placerat, metus quam auctor nunc, in consectetur orci dui quis ligula.',
+    numberOfLinesToShow: 3,
+  },
 } satisfies Meta<typeof CollapsibleText>;
 
 export default meta;
@@ -13,10 +18,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Collapsed = {
   args: {
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus scelerisque, nibh eu dapibus dignissim, eros orci tristique ipsum, id interdum ex neque a dui. Quisque pretium aliquam dui, pulvinar venenatis sem consectetur non. Sed felis mi, viverra sit amet blandit sed, vestibulum eu purus. Nulla eget tellus sodales, volutpat nulla in, blandit tellus. Fusce congue vitae elit ac scelerisque. Maecenas fringilla ipsum in enim volutpat dignissim. Aenean convallis urna vel nibh semper faucibus. Nunc non pellentesque nibh. Sed tempor, erat sit amet volutpat placerat, metus quam auctor nunc, in consectetur orci dui quis ligula.',
     collapsed: true,
-    numberOfLinesToShow: 3,
   },
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
@@ -31,10 +33,7 @@ export const Collapsed = {
 
 export const Uncollapsed = {
   args: {
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus scelerisque, nibh eu dapibus dignissim, eros orci tristique ipsum, id interdum ex neque a dui. Quisque pretium aliquam dui, pulvinar venenatis sem consectetur non. Sed felis mi, viverra sit amet blandit sed, vestibulum eu purus. Nulla eget tellus sodales, volutpat nulla in, blandit tellus. Fusce congue vitae elit ac scelerisque. Maecenas fringilla ipsum in enim volutpat dignissim. Aenean convallis urna vel nibh semper faucibus. Nunc non pellentesque nibh. Sed tempor, erat sit amet volutpat placerat, metus quam auctor nunc, in consectetur orci dui quis ligula, metus quam auctor nunc, in consectetur orci dui quis ligula.',
     collapsed: false,
-    numberOfLinesToShow: 3,
   },
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
