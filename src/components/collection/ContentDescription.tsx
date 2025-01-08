64916c7f9e0e5f1fa297883eb7118551697a37ab
Kim Lan Phan Hoang <pyphilia@gmail.com>
2025-01-08 13:35:45 +0100

    feat: add storybook and collapsible text (#732)
    * feat: add storybook and collapsible text
    
    * refactor: apply PR requested changes

--- src/components/collection/ContentDescription.tsx
+++ src/components/collection/ContentDescription.tsx
@@ -1,75 +1,26 @@
-import { Interweave } from 'interweave';
 import 'katex/dist/katex.min.css';
 
-import React from 'react';
 import 'react-quill/dist/quill.snow.css';
 
-import { Box, styled } from '@mui/material';
-
-type CollapsibleDescriptionProps = {
-  collapsed: boolean;
-  numberOfLinesToShow?: number;
-  children: JSX.Element;
-};
-
-export const CollapsibleDescription = ({
-  collapsed,
-  numberOfLinesToShow = 1,
-  children,
-}: CollapsibleDescriptionProps) => (
-  <Box
-    sx={{
-      display: '-webkit-box',
-      overflow: 'hidden',
-      // number of lines to show
-      WebkitLineClamp: collapsed ? numberOfLinesToShow : 'unset',
-      WebkitBoxOrient: 'vertical',
-      '& > p': {
-        margin: 0,
-      },
-    }}
-  >
-    {children}
-  </Box>
-);
-
-const StyledDiv = styled('div')({
-  '& .ql-editor': {
-    paddingLeft: '0px !important',
-    '& p': {
-      paddingBottom: 3,
-      paddingTop: 3,
-    },
-  },
-});
+import {
+  CollapsibleText,
+  type CollapsibleTextProps,
+} from '../common/CollapsibleText/CollapsibleText';
 
 type Props = {
   content: string;
-  collapsed?: CollapsibleDescriptionProps['collapsed'];
-  numberOfLinesToShow?: CollapsibleDescriptionProps['numberOfLinesToShow'];
+  collapsed?: CollapsibleTextProps['collapsed'];
+  numberOfLinesToShow?: CollapsibleTextProps['numberOfLinesToShow'];
 };
 const ContentDescription = ({
   content,
   collapsed = true,
   numberOfLinesToShow,
 }: Props): JSX.Element => (
-  <StyledDiv className="quill">
-    <div className="ql-snow ql-disabled">
-      <div className="ql-editor" style={{ padding: '0px' }}>
-        <CollapsibleDescription
-          collapsed={collapsed}
-          numberOfLinesToShow={numberOfLinesToShow}
-        >
-          <Interweave
-            content={content}
-            // onAfterParse={(arr) => (collapsed ? [arr.join(' ')] : arr)}
-            // disable html output when description is collapsed
-            // noHtml={collapsed}
-            noWrap
-          />
-        </CollapsibleDescription>
-      </div>
-    </div>
-  </StyledDiv>
+  <CollapsibleText
+    content={content}
+    collapsed={collapsed}
+    numberOfLinesToShow={numberOfLinesToShow}
+  />
 );
 export default ContentDescription;
---
+++ src/components/common/CollapsibleText/CollapsibleText.stories.tsx
@@ -0,0 +1,47 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { expect, within } from '@storybook/test';
+
+import { CollapsibleText } from './CollapsibleText';
+
+const meta = {
+  title: 'Common/CollapsibleText',
+  component: CollapsibleText,
+  args: {
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus scelerisque, nibh eu dapibus dignissim, eros orci tristique ipsum, id interdum ex neque a dui. Quisque pretium aliquam dui, pulvinar venenatis sem consectetur non. Sed felis mi, viverra sit amet blandit sed, vestibulum eu purus. Nulla eget tellus sodales, Vivamus scelerisque, nibh eu dapibus dignissim, eros orci tristique ipsum, id interdum ex neque a dui. Quisque pretium aliquam dui, pulvinar venenatis sem consectetur non. Sed felis mi, viverra sit amet blandit sed, vestibulum eu purus. Nulla eget tellus sodales, volutpat nulla in, blandit tellus. Fusce congue vitae elit ac scelerisque. Maecenas fringilla ipsum in enim volutpat dignissim. Aenean convallis urna vel nibh semper faucibus. Nunc non pellentesque nibh. Sed tempor, erat sit amet volutpat placerat, metus quam auctor nunc, in consectetur orci dui quis ligula.',
+    numberOfLinesToShow: 3,
+  },
+} satisfies Meta<typeof CollapsibleText>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Collapsed = {
+  args: {
+    collapsed: true,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await expect(canvas.getByText('Lorem', { exact: false })).toBeVisible();
+
+    await expect(
+      canvas.getByText('Lorem', { exact: false }).offsetHeight,
+    ).toBeLessThan(100);
+  },
+} satisfies Story;
+
+export const Uncollapsed = {
+  args: {
+    collapsed: false,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await expect(canvas.getByText('Lorem', { exact: false })).toBeVisible();
+
+    await expect(
+      canvas.getByText('Lorem', { exact: false }).offsetHeight,
+    ).toBeGreaterThan(100);
+  },
+} satisfies Story;
