b3f5efee4f74e666f06d7b9dd811210ee9d23841
spaenleh <spaenleh@gmail.com>
2025-07-17 09:00:28 +0200

    fix: update favicon to use purple one

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -44,6 +44,19 @@ export const Route = wrapCreateRootRouteWithSentry(
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     ],
     links: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon-96x96.png',
+        sizes: '96x96',
+      },
+      { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+      { rel: 'shortcut icon', href: '/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
       { rel: 'stylesheet', href: fontsourceVariableNunitoCss },
       {
         rel: 'stylesheet',
