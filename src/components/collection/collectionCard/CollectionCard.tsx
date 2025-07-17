706ec1d43546ef28cfe21df295c0c6ba9a4a1933
Basile Spaenlehauer <spaenleh@gmail.com>
2025-07-17 12:15:04 +0200

    fix: use sort instead of toSorted for compatibility (#808)

--- src/components/collection/collectionCard/CollectionCard.tsx
+++ src/components/collection/collectionCard/CollectionCard.tsx
@@ -9,6 +9,23 @@ import { BigCard } from '../../common/Card/BigCard';
 import { MemberAvatar } from '../../common/Card/MemberAvatar';
 import { ItemTag } from './ItemTag';
 
+function mapTags(collection: ItemOrSearchedItem) {
+  const unsortedTags = Object.values(TagCategory).flatMap((category) => {
+    if (`${category}` in collection) {
+      return (
+        collection[category].map((c) => ({ id: c, category, name: c })) ?? []
+      );
+    }
+    return [];
+  });
+  // shallow copy of unsortedTags
+  const tags = [...unsortedTags];
+  // sort in place. Can't use .toSorted() because it is a bit too new.
+  // ref: https://github.com/graasp/graasp-library/issues/807
+  tags.sort((a, b) => (a > b ? 1 : -1));
+  return tags;
+}
+
 type Props = {
   collection: ItemOrSearchedItem;
   showIsContentTag?: boolean;
@@ -36,16 +53,7 @@ export const CollectionCard = ({
 
   const link = buildCollectionRoute(id);
 
-  const tags = Object.values(TagCategory)
-    .flatMap((category) => {
-      if (`${category}` in collection) {
-        return (
-          collection[category].map((c) => ({ id: c, category, name: c })) ?? []
-        );
-      }
-      return [];
-    })
-    .toSorted((a, b) => (a > b ? 1 : -1));
+  const tags = mapTags(collection);
 
   // warning: discriminated item does not have likes, when it is used on the homepage
   const likes = 'likes' in collection ? collection.likes : 0;
